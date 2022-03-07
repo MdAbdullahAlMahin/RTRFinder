303207d4e09fcef733812a0a9c906fc74acf1eaf
Shoh4g <u3572970@connect.hku.hk>
2022-03-07 18:42:58 +0600

    updated

--- Trial/App.js
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-import Navigator from "./screens/routes/HomeStack";
-import WelcomeScreen from "./screens/welcomescreen";
-export default function App() {
-  return <Navigator />;
-}
