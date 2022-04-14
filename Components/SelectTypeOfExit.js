1feece2b22c30f5f002bece081dddeaa7dfc6dfa
racheliscoding <100477966+racheliscoding@users.noreply.github.com>
2022-04-15 00:31:40 +0800

    Add files via upload

--- Components/SelectTypeOfExit.js
+++ Components/SelectTypeOfExit.js
@@ -40,19 +40,19 @@ const SelectTypeOfExit = ({ route, navigation }) => {
     var map = useMap(station, stepSize);
     var dir = map.findExit(exit, "Escalator");
     navigation.navigate("Directions", { dir, stepSize, station, exit });
-    Speech.speak("Escalator");
+    Speech.speak("Escalator, Now step out of the escalator and start your journey ");
   };
   const navigateEl = () => {
     var map = useMap(station, stepSize);
     var dir = map.findExit(exit, "Elevator");
     navigation.navigate("Directions", { dir, stepSize, station, exit });
-    Speech.speak("Elevator");
+    Speech.speak("Elevator, Now step out of the escalator and start your journey");
   };
   const navigateSt = () => {
     var map = useMap(station, stepSize);
     var dir = map.findExit(exit, "Stairs");
     navigation.navigate("Directions", { dir, stepSize, station, exit });
-    Speech.speak("Stairs");
+    Speech.speak("Stairs, Now step out of the escalator and start your journey");
   };
   return (
     <SafeAreaView style={styles.container}>
